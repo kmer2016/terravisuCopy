a513bb9bd360e00e1989dab1a2f8dceaae6cf388
chaka <ngamenichaka@yahoo.fr>
2023-07-25 09:49:48 +0200

    fix: extract error message

--- src/modules/railway/core/usecases/fetch-railways.usecase.ts
+++ src/modules/railway/core/usecases/fetch-railways.usecase.ts
@@ -1,5 +1,6 @@
 import { Dependencies } from "../../../store/dependencies";
 import { AppDispatch, AppGetState } from "../../../store/store";
+import { extractErrorMessage } from "../../../utils/error.utils";
 import { railwailSlice } from "../store/railway.slice";
 
 export const fetchRailways = async (
@@ -12,6 +13,6 @@ export const fetchRailways = async (
             const railways = await dependencies.railwayGateway.getRailways();
             dispatch(railwailSlice.actions.storeRailways(railways));
         } catch (error) {
-            dispatch(railwailSlice.actions.handleRailwaysError(error.message));
+            dispatch(railwailSlice.actions.handleRailwaysError(extractErrorMessage(error)));
         }
 }
